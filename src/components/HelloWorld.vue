9cac897438f15ea8ffe7574fc681f2404f2a6fbf
Sukanya <sukanyarath1987@gmail.com>
2024-09-26 19:02:42 -0700

    few fixes around missing await and adding audience check along with client roles check.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 import { logout, _kc } from '../services/keycloak'
+
 </script>
 
 <template>
   
-  <h1>Welcome {{ _kc.tokenParsed?.display_name }} {{ _kc.tokenParsed?.resource_access }}</h1><button @click="logout">logout</button>
+  <h1>Welcome {{ _kc.tokenParsed?.display_name }}, roles you have are {{ _kc.tokenParsed?.client_roles }}</h1><button @click="logout">logout</button>
 
 </template>
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,26 +1,35 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import HelloWorld from '../components/HelloWorld.vue'
-import { initializeKeycloak } from '../services/keycloak'
+import {initializeKeycloak} from '../services/keycloak'
+import Keycloak from "keycloak-js";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HelloWorld,
-      meta: {
-        requiresAuth: true,
-      },
-    },
-  ],
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: HelloWorld,
+            meta: {
+                requiresAuth: true,
+            },
+        },
+    ],
 })
 
 router.beforeEach(async (to, _from) => {
-  if (to.meta.requiresAuth) {
-    const keycloak = await initializeKeycloak()
-    if (keycloak?.authenticated) return true
-  } else return false
+
+
+    if (to.meta.requiresAuth) {
+        const keycloak = await initializeKeycloak()
+        if (isValidUser(keycloak)) return true
+    } else return false
 })
+
+function isValidUser(keycloak: Keycloak) {
+    return keycloak?.authenticated
+        && keycloak.tokenParsed?.aud === 'fin-tbs-btf-5747'
+        && keycloak.tokenParsed?.client_roles?.includes('fin-tbs-btf-admin');
+}
 
 export default router
