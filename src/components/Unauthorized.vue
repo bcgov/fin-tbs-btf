e7e6f5d9ea0d5a7790a8dd2ed4064e7a91e6128f
Sukanya <sukanyarath1987@gmail.com>
2024-09-26 19:54:41 -0700

    added role check

---
+++ src/components/Unauthorized.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  
+  <h1>You are not authorized to access the application.</h1>
+
+</template>
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,7 @@ import {createRouter, createWebHistory} from 'vue-router'
 import HelloWorld from '../components/HelloWorld.vue'
 import {initializeKeycloak} from '../services/keycloak'
 import Keycloak from "keycloak-js";
+import Unauthorized from "../components/Unauthorized.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,16 +15,35 @@ const router = createRouter({
                 requiresAuth: true,
             },
         },
+        {
+            path: '/unauthorized',
+            name: 'unauthorized',
+            component: Unauthorized,
+            meta: {
+                requiresAuth: false,
+            },
+        }
+
     ],
 })
 
-router.beforeEach(async (to, _from) => {
+router.beforeEach(async (to, _from, next) => {
 
 
     if (to.meta.requiresAuth) {
         const keycloak = await initializeKeycloak()
-        if (isValidUser(keycloak)) return true
-    } else return false
+        if (isValidUser(keycloak)) {
+            next();
+            console.log('valid user')
+            return;
+        } else {
+            console.log('unauthorized user')
+            next('/unauthorized');
+        }
+    } else {
+        console.log('open page')
+        next();
+    }
 })
 
 function isValidUser(keycloak: Keycloak) {
