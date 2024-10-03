779f8a4048283114308d115f649ece869e4e3cd9
Brock Anderson <brock@bandersgeo.ca>
2024-10-03 16:56:03 -0700

    routing, auth, page layout

---
+++ src/components/DashboardPage.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-row dense >
+    <v-col class="d-flex justify-start">
+      <h5>Dashboard</h5>
+    </v-col>
+  </v-row>
+  
+  <v-row dense>
+    <v-col class="d-flex justify-start">
+      <v-btn class="btn-primary" to="upload" v-if="!isAuthenticated">Login</v-btn>
+      <v-btn class="btn-primary" to="upload" v-if="isAuthenticated">Upload files</v-btn>
+    </v-col>
+  </v-row>
+</template>
+
+<script setup lang="ts">
+import router from '../routes';
+import { useAuthStore } from '../stores/auth-store';
+import { onMounted } from 'vue';
+import { storeToRefs } from 'pinia';
+
+const authStore = useAuthStore();
+const {
+  isAuthenticated
+} = storeToRefs(authStore);
+
+onMounted(() => {
+  console.log(`authStore.isAuthorized: ${authStore.isAuthorized}`)
+  //if (authStore.isAuthorized) {
+  //  router.push('/upload')
+  //}
+});
+
+function login() {
+  authStore.init()
+}
+
+</script>
---
+++ src/components/UploadPage.vue
@@ -0,0 +1,12 @@
+<template>
+ <v-row dense >
+    <v-col class="d-flex justify-start">
+      <h5>Upload files</h5>
+    </v-col>
+  </v-row>
+    
+</template>
+
+<script setup lang="ts">
+
+</script>
