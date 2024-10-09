852dfe2fd2dc988b10f80bc96d4009340e5bbe8a
Brock Anderson <brock@bandersgeo.ca>
2024-10-09 16:36:12 -0700

    fixed broken tests

--- src/services/__tests__/pdf-service.spec.ts
+++ src/services/__tests__/pdf-service.spec.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect, vi, beforeEach } from "vitest";
+import { beforeEach, describe, expect, it, vi } from "vitest";
 import pdfService from "../pdf-service";
 
 // Mock the web worker because "Web Workers are not supported in this environment."
@@ -10,14 +10,16 @@ vi.mock("pdfjs-dist", () => ({
   getDocument: (...args: any) => mockGetDocument(...args),
 }));
 
+const mockPdfFile = new File([""], "filename.pdf", {
+  type: "application/pdf",
+});
+
 describe("pdfService", () => {
   beforeEach(() => {
     vi.resetAllMocks();
   });
 
   it("should handle parsing PDF and extracting form fields", async () => {
-    const mockArrayBuffer = new ArrayBuffer(8);
-
     // Mock getDocument and PDF document
     const mockPdfDoc = {
       numPages: 2,
@@ -33,7 +35,7 @@ describe("pdfService", () => {
     });
 
     // Run the service
-    const fields = await pdfService.parsePDF(mockArrayBuffer);
+    const fields = await pdfService.parsePDF(mockPdfFile);
 
     // Assertions
     expect(fields).toEqual({
@@ -41,58 +43,15 @@ describe("pdfService", () => {
       field2: "value2",
     });
 
-    expect(mockGetDocument).toHaveBeenCalledWith({ data: mockArrayBuffer });
+    const arrayBuffer = await mockPdfFile.arrayBuffer();
+    expect(mockGetDocument).toHaveBeenCalledWith({
+      data: arrayBuffer,
+    });
     expect(mockPdfDoc.getPage).toHaveBeenCalledWith(1);
     expect(mockPdfDoc.getPage).toHaveBeenCalledWith(2);
   });
 
-  it("should handle multiple files", async () => {
-    // Mock FileList and File
-    const mockFile = new File(["%PDF"], "mock.pdf", {
-      type: "application/pdf",
-    });
-    const mockFiles = {
-      length: 1,
-      item: () => mockFile,
-      0: mockFile,
-    } as unknown as FileList;
-
-    // Mock arrayBuffer
-    const mockArrayBuffer = new ArrayBuffer(8);
-    mockFile.arrayBuffer = vi.fn().mockResolvedValue(mockArrayBuffer);
-
-    // Mock getDocument and PDF document
-    const mockPdfDoc = {
-      numPages: 1,
-      getPage: vi.fn().mockResolvedValue({
-        getAnnotations: vi
-          .fn()
-          .mockResolvedValue([
-            { fieldName: "field1", fieldValue: "value1", fieldType: "Tx" },
-          ]),
-      }),
-    };
-    mockGetDocument.mockReturnValue({
-      promise: Promise.resolve(mockPdfDoc),
-    });
-
-    // Run the service
-    const result = await pdfService.handleFiles(mockFiles);
-
-    // Assertions
-    expect(result).toHaveLength(1);
-    expect(result[0]).toEqual({
-      field1: "value1",
-    });
-
-    expect(mockFile.arrayBuffer).toHaveBeenCalledTimes(1);
-    expect(mockGetDocument).toHaveBeenCalledWith({ data: mockArrayBuffer });
-    expect(mockPdfDoc.getPage).toHaveBeenCalledWith(1);
-  });
-
   it('should handle parsing choice fields (fieldType: "Ch")', async () => {
-    const mockArrayBuffer = new ArrayBuffer(8);
-
     // Mock choice field annotation with options
     const mockPdfDoc = {
       numPages: 1,
@@ -115,20 +74,19 @@ describe("pdfService", () => {
     });
 
     // Run the service
-    const fields = await pdfService.parsePDF(mockArrayBuffer);
+    const fields = await pdfService.parsePDF(mockPdfFile);
 
     // Assertions: Ensure the correct option is selected
     expect(fields).toEqual({
       dropdownField: "Option 2", // This should match the displayValue of the selected option
     });
 
-    expect(mockGetDocument).toHaveBeenCalledWith({ data: mockArrayBuffer });
+    const arrayBuffer = await mockPdfFile.arrayBuffer();
+    expect(mockGetDocument).toHaveBeenCalledWith({ data: arrayBuffer });
     expect(mockPdfDoc.getPage).toHaveBeenCalledWith(1);
   });
 
   it("should handle empty choice", async () => {
-    const mockArrayBuffer = new ArrayBuffer(8);
-
     // Mock choice field annotation with options
     const mockPdfDoc = {
       numPages: 1,
@@ -151,27 +109,26 @@ describe("pdfService", () => {
     });
 
     // Run the service
-    const fields = await pdfService.parsePDF(mockArrayBuffer);
+    const fields = await pdfService.parsePDF(mockPdfFile);
 
     // Assertions: Ensure the correct option is selected
     expect(fields).toEqual({
       dropdownField: undefined, // This should match the displayValue of the selected option
     });
 
-    expect(mockGetDocument).toHaveBeenCalledWith({ data: mockArrayBuffer });
+    const arrayBuffer = await mockPdfFile.arrayBuffer();
+    expect(mockGetDocument).toHaveBeenCalledWith({ data: arrayBuffer });
     expect(mockPdfDoc.getPage).toHaveBeenCalledWith(1);
   });
 
   it("should handle error during PDF parsing", async () => {
-    const mockArrayBuffer = new ArrayBuffer(8);
-
     // Mock getDocument to throw an error
     mockGetDocument.mockReturnValue({
       promise: Promise.reject(new Error("PDF parsing error")),
     });
 
     // Catch the error
-    await expect(pdfService.parsePDF(mockArrayBuffer)).rejects.toThrow(
+    await expect(pdfService.parsePDF(mockPdfFile)).rejects.toThrow(
       "PDF parsing error",
     );
   });
