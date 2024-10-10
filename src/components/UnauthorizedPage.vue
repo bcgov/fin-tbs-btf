77213e37684a29a518e3f989e9670de4f6c01cff
jer3k <99355997+jer3k@users.noreply.github.com>
2024-10-10 16:06:29 -0700

    unauthorized ui (#16)

--- src/components/UnauthorizedPage.vue
+++ src/components/UnauthorizedPage.vue
@@ -1,5 +1,19 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h5>You are not authorized to access the application.</h5>
+  <v-row dense class="w-100 h-100">
+    <v-col class="d-flex justify-center align-center w-100 h-100">
+      <v-card class="login-card bg-grey-lighten-5">
+        <v-card-text
+          class="h-100 d-flex flex-column justify-center align-center"
+        >
+          <div>
+            <h2 class="ma-5">
+              You are not authorized to access the application.
+            </h2>
+          </div>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
