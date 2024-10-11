0f7a428430cc656aef54b87b63f0e8e5789faad0
Brock Anderson <brock@bandersgeo.ca>
2024-10-10 17:20:58 -0700

    feat: handle unauthorized user (#18)
    * improvements to the 'upload files' page.  drag-and-drop now supported.
    
    * improved the login flow.  updated the login page.
    
    * added an alert prompting to remove the invalid files
    
    * slight text change
    
    * on the login page: if the user is already logged in, then the 'login' just directs the user into the app
    
    * distinguish between not authorized and not authenticated

--- src/components/UnauthorizedPage.vue
+++ src/components/UnauthorizedPage.vue
@@ -1,5 +1,3 @@
-<script setup lang="ts"></script>
-
 <template>
   <v-row dense class="w-100 h-100">
     <v-col class="d-flex justify-center align-center w-100 h-100">
@@ -8,12 +6,21 @@
           class="h-100 d-flex flex-column justify-center align-center"
         >
           <div>
-            <h2 class="ma-5">
+            <h2 class="mb-2">
               You are not authorized to access the application.
             </h2>
+            <v-btn class="btn-primary" @click="authStore.logout()">
+              Logout
+            </v-btn>
           </div>
         </v-card-text>
       </v-card>
     </v-col>
   </v-row>
 </template>
+
+<script setup lang="ts">
+import { useAuthStore } from "../stores/auth-store";
+
+const authStore = useAuthStore();
+</script>
