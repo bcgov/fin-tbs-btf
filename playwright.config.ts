3918f25c21dd8a0d4545c90d58f234c1f5ef49a9
jer3k <99355997+jer3k@users.noreply.github.com>
2024-12-03 15:58:12 -0800

    e2e github action

--- playwright.config.ts
+++ playwright.config.ts
@@ -59,35 +59,49 @@ export default defineConfig({
       dependencies: ["setup"],
       teardown: "teardown",
     },
+    {
+      name: "Google Chrome",
+      use: {
+        ...devices["Desktop Chrome"],
+        channel: "chrome",
+        baseURL: baseURL,
+        storageState: "user.json",
+      },
+      dependencies: ["setup"],
+      teardown: "teardown",
+    },
 
-    // {
-    //   name: "firefox",
-    //   use: { ...devices["Desktop Firefox"] },
-    // },
+    {
+      name: "firefox",
+      use: {
+        ...devices["Desktop Firefox"],
+        baseURL: baseURL,
+        storageState: "user.json",
+      },
+      dependencies: ["setup"],
+      teardown: "teardown",
+    },
 
-    // {
-    //   name: "webkit",
-    //   use: { ...devices["Desktop Safari"] },
-    // },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
+    {
+      name: "safari",
+      use: {
+        ...devices["Desktop Safari"],
+        baseURL: baseURL,
+        storageState: "user.json",
+      },
+      dependencies: ["setup"],
+      teardown: "teardown",
+    },
+    {
+      name: "Microsoft Edge",
+      use: {
+        ...devices["Desktop Edge"],
+        channel: "msedge",
+        baseURL: baseURL,
+        storageState: "user.json",
+      },
+      dependencies: ["setup"],
+      teardown: "teardown",
+    },
   ],
 });
