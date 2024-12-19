addcc49c3c077c84242f057dfd0201bcce14e38d
jer3k <99355997+jer3k@users.noreply.github.com>
2024-12-19 11:09:30 -0800

    unit tests for data

--- __mocks__/pdfjs-dist.ts
+++ __mocks__/pdfjs-dist.ts
@@ -1,24 +1,52 @@
 import { vi } from "vitest";
 
-// let mockAnnotations = [
-//   { id: 1, type: "annotation", content: "Default Annotation 1" },
-//   { id: 2, type: "annotation", content: "Default Annotation 2" },
-// ];
+const mockGetAnnotations = vi.fn(() => [
+  { fieldName: "field3", fieldValue: "value3", fieldType: "Tx" },
+  { fieldName: "field4", fieldValue: "value4", fieldType: "Tx" },
+]);
 
-// export const setMockAnnotations = (annotations) => {
-//   mockAnnotations = annotations;
-// };
-
-// __mocks__/pdfjs-dist.ts
-export const getDocument = vi.fn(() => ({
-  getPage: vi.fn(() =>
-    Promise.resolve({
-      getAnnotations: vi.fn(() =>
-        Promise.resolve([
-          { id: 1, type: "annotation", content: "Default Annotation 1" },
-          { id: 2, type: "annotation", content: "Default Annotation 2" },
-        ]),
-      ),
-    }),
-  ),
+const mockGetPage = vi.fn((pageIndex) => ({
+  getAnnotations: mockGetAnnotations,
 }));
+
+const mockGetDocument = vi.fn(() => ({
+  promise: Promise.resolve({
+    numPages: 1,
+    getPage: mockGetPage,
+  }),
+}));
+
+export const getDocument = mockGetDocument;
+
+export const PdfjsTestHelpers = {
+  /** Spies for testing.
+   * Must call vi.restoreAllMocks(); afterEach test. */
+  mock: {
+    getDocument: mockGetDocument,
+    getPage: mockGetPage,
+    getAnnotations: mockGetAnnotations,
+  },
+
+  /** Mock the annotations of a single page pdf document. */
+  setMockAnnotations: (annotations: any[]) => {
+    mockGetAnnotations.mockResolvedValue(annotations);
+  },
+
+  /** Mock the annotations of a multi-page pdf document. */
+  setPagesOfMockAnnotations: (annotations: any[][]) => {
+    mockGetPage.mockImplementation((pageIndex) => ({
+      getAnnotations: mockGetAnnotations.mockResolvedValue(
+        annotations[pageIndex - 1] || [],
+      ),
+    }));
+
+    mockGetDocument.mockReturnValue({
+      promise: Promise.resolve({
+        get numPages() {
+          return annotations.length;
+        },
+        getPage: mockGetPage,
+      }),
+    });
+  },
+};
